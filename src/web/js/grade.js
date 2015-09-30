ea744281f4ac38b4494ff5edbef41ded4950ea56
Frank Goodman <frank_goodman@brown.edu>
2015-09-30 10:40:20 -0400

    run a file on gdrive

--- src/web/js/grade.js
+++ src/web/js/grade.js
@@ -3,14 +3,24 @@ $(function() {
   require(["/js/repl-ui.js", "/js/web-runner.js",
   "/js/editor-find-module.js"], function(_, webRunner,
   find) {
+    // TODO(all): Move createPCAPI to a require module.
+    var storageAPIP = createProgramCollectionAPI(
+      clientId, apiKey, "code.pyret.org", false);
+
     var proxy = function(s) {
       return APP_BASE_URL + "/downloadImg?" + s;
     };
     var makeFind = find.createFindModule(null);
     var runnerP = webRunner.createRunner(proxy, makeFind);
-    var resultP = runnerP.then(function(runner) {
-      return runner.runString("print(link(1, empty))", "test");
-    });
+    var resultP = Q.all([runnerP, storageAPIP]).spread(
+      function(runner, storageAPI) {
+        var fileP = storageAPI.api.getFileById("0B-_f7M_B5NMiV0M2ckNhTE5FdGc");
+        return fileP.then(function(file) {
+          return file.getContents().then(function(contents) {
+            return runner.runString(contents, "test");
+          });
+        });
+      });
     resultP.then(function(result) { console.log(result); });
     resultP.fail(function(exn) { console.log(exn); });
   });
